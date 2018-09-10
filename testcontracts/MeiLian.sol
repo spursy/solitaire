1f61c1db2a9d84c5c781197dd3ad1e5ebd1e4c54
yangyang <yang.yang@gloritysolutions.com>
2018-09-10 15:07:53 +0800

    update

--- testcontracts/MeiLian.sol
+++ testcontracts/MeiLian.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.23;
 
 /**
  * @title SafeMath
@@ -37,7 +37,7 @@ library SafeMath {
  */
 contract ERC20Basic {
     uint256 public totalSupply;
-    function balanceOf(address who) public constant returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
     function transfer(address to, uint256 value) public returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
@@ -82,7 +82,7 @@ contract BasicToken is ERC20Basic {
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public constant returns (uint256);
+    function allowance(address owner, address spender) public view returns (uint256);
     function transferFrom(address from, address to, uint256 value) public returns (bool);
     function approve(address spender, uint256 value) public returns (bool);
     event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -227,7 +227,7 @@ contract Pausable is Ownable {
     */
     function unpause() onlyOwner whenPaused public {
         paused = false;
-        Unpause();
+        emit Unpause();
     }
 }
 
