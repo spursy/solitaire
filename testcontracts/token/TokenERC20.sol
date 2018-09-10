4ccf9913531e9da18491212aa53c63406a4c1cbe
spursyy <email@example.com>
2018-09-10 20:55:27 +0800

    add files

---
+++ testcontracts/token/TokenERC20.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.23;
+interface tokenRecipient {
+    function receiveApproval(address _from, uint _value, address _token, bytes _extraData) external;
+}
+
+contract TokenERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimal = 18;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Burn(address indexed from, uint256 value);
+
+    constructor(uint256 initialSupply, string tokenName, string tokenSymbol) public {
+        totalSupply = initialSupply * uint256(decimal);
+        balanceOf[msg.sender] = totalSupply;
+        name = tokenName;
+        symbol = tokenSymbol;
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal{
+        require(_to != 0x0, "target address is 0x0");
+
+        require(balanceOf[_from] >= _value, "balance is not enough.");
+        require(balanceOf[_to] + _value > balanceOf[_to], "total balance is not matching.");
+
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender], "balance is not enough.");
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success){
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "balance is not enough.");
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value, "balance is not enough.");
+        require(_value <= allowance[_from][msg.sender], "balance is not enough");
+
+        totalSupply -= _value;
+        emit Burn(_from, _value);
+        return true;
+    }
+}
